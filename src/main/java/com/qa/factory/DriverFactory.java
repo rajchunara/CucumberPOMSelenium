b481bb1ce05f93e836c754ac749e9756d23c4698
rajchunara <rajchunara777@gmail.com>
2021-02-16 22:46:21 -0500

    created DriverFactory to get thread safe webdriver

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -1,5 +1,51 @@
 package com.qa.factory;
 
-public class DriverFactory {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+	
+	public WebDriver driver;
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	
+	
+	/*
+	 * This method is used to initialize the web driver based on the argument passed
+	 * @param browser
+	 * @return WebDriver 	
+	 */
+	public WebDriver init_driver(String browser) {
+		System.out.println("browser value is "+browser);
+		
+		if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();	
+			
+			tlDriver.set(new ChromeDriver());
+		}else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver());
+		}else {
+			System.out.println("Please pass correct browser value"+ browser);
+		}
+		
+		performInitialTasksOnWebDriver();
+		
+		return getDriver();
+		
+	}
+	
+	
+	
+	public static synchronized WebDriver getDriver() {
+		return tlDriver.get();
+	}
+	
+	public void performInitialTasksOnWebDriver() {
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+	}
+	
 }
