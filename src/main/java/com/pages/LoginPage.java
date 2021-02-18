c95a3ac93e8e2e0aa1a768c3f1ef78d249abebe6
rajchunara <rajchunara777@gmail.com>
2021-02-17 22:21:32 -0500

    login page steps added

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -1,5 +1,58 @@
 package com.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+	
+	private WebDriver driver;
+	
+	// By locators
+	private By usernameElement = By.id("user-name");
+	private By passwordElement = By.id("password");	
+	private By loginButton = By.id("login-button");
+	private By acceptedUsernameHeader=By.xpath("//h4[text()=\"Accepted usernames are:\"]");
+	private By acceptedPasswordHeader = By.xpath("//h4[text()=\"Password for all users:\"]");
+	private By productsHeader = By.xpath("//div[text()=\"Products\"]");
+	
+	
+	//constructor
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		
+	}
+	
+	//page actions(methods)
+	public String getLoginPageTitle() {
+		return driver.getTitle();
+	}
+	
+	public boolean getAcceptedUsernameHeader() {
+		return driver.findElement(acceptedUsernameHeader).isDisplayed();
+	}
+	
+	public boolean getPasswordHeader() {
+		return driver.findElement(acceptedPasswordHeader).isDisplayed();
+	}
+	
+	
+	public void enterUsername(String username) {
+		driver.findElement(usernameElement).sendKeys(username);
+	}
+	
+	public void enterPassword(String password) {
+		driver.findElement(passwordElement).sendKeys(password);
+	}
+	
+	public void clickLoginButton() {
+		driver.findElement(loginButton).click();
+	}
+	
+	public boolean isProductsPageDisplayed() {
+		return driver.findElement(productsHeader).isDisplayed();
+	}
+	
+	
+	
 
 }
--- src/test/java/stepdefinations/LoginPageSteps.java
+++ src/test/java/stepdefinations/LoginPageSteps.java
@@ -1,64 +1,65 @@
 package stepdefinations;
 
+import com.pages.LoginPage;
+import com.qa.factory.DriverFactory;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import junit.framework.Assert;
 
 public class LoginPageSteps {
+	
+	private LoginPage loginPage= new LoginPage(DriverFactory.getDriver());
+	private static String pageTitle;
 	
 
 @Given("user is on login page")
 public void user_is_on_login_page() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	DriverFactory.getDriver().get("https://www.saucedemo.com/index.html");
 }
+
 
 @When("user gets the title of the page")
 public void user_gets_the_title_of_the_page() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	pageTitle = loginPage.getLoginPageTitle();
+
 }
 
 @Then("page title should be {string}")
-public void page_title_should_be(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+public void page_title_should_be(String expectedPageTitle) {
+	
+	Assert.assertTrue(pageTitle.equals(expectedPageTitle));
 }
 
 @Then("accepted usernames should be available with {string}")
 public void accepted_usernames_should_be_available_with(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	Assert.assertTrue(loginPage.getAcceptedUsernameHeader());
 }
 
 @Then("accepted password should be available with {string}")
 public void accepted_password_should_be_available_with(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	Assert.assertTrue(loginPage.getPasswordHeader());
 }
 
 @When("user enters username {string}")
-public void user_enters_username(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+public void user_enters_username(String username) {
+	loginPage.enterUsername(username);
 }
 
 @When("user enters password {string}")
-public void user_enters_password(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+public void user_enters_password(String password) {
+	loginPage.enterPassword(password);
 }
 
 @When("user clicks on Login button")
 public void user_clicks_on_login_button() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	loginPage.clickLoginButton();
 }
 
 @Then("products page appear with header {string}")
 public void products_page_appear_with_header(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	Assert.assertTrue(loginPage.isProductsPageDisplayed());
 }
 
 
